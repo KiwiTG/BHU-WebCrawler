df674dbf3cbafeef62f675684e23f5ab17e2f3db
H4Z3 <93849885+KiwiTG@users.noreply.github.com>
2022-10-27 13:23:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests, os, argparse, colorama, shodan, socket, urllib.request, threading, concurrent.futures
 from colorama import Fore
 
-SHODAN_API_KEY = 'AaeWA2DtLpJM4H0ktOXui6IkoTElZjoE'
+SHODAN_API_KEY = ''
 api = shodan.Shodan(SHODAN_API_KEY)
 
 parser = argparse.ArgumentParser(description='BHU Web Crawler')
